23fcef2d95dd67b7778a423d09975b18245fd771
James Jilhaney <notail2111@gmail.com>
2023-07-13 11:08:03 +0800

    Added new attributes to lifeitem class

--- lib/models/life_item.dart
+++ lib/models/life_item.dart
@@ -1,17 +1,22 @@
 // This will serve as the model for the life items
 // Life items are items that are in the user's bucket list
+import 'package:diem/models/life_item_category.dart';
 
 class LifeItem {
+  String id;
   String title;
   String description;
   bool isCompleted;
+  List<LifeItemCategory>? categories;
   String? location;
   String? image;
 
   LifeItem(
-      {required this.title,
+      {required this.id,
+      required this.title,
       required this.description,
       required this.isCompleted,
+      this.categories,
       this.location,
       this.image});
 }
