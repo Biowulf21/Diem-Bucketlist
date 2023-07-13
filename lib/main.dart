28899efba1d0e00f5662ccd0876c6d43124249c0
James Jilhaney <notail2111@gmail.com>
2023-07-13 10:15:18 +0800

    Created working bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -43,45 +43,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _currentNavBarIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final bottomNavBarItems = <BottomNavigationBarItem>[
+    BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+    BottomNavigationBarItem(icon: Icon(Icons.map), label: "Map"),
+    BottomNavigationBarItem(icon: Icon(Icons.people), label: "People"),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    const bottomNavBarItems = <BottomNavigationBarItem>[
-      BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-      BottomNavigationBarItem(icon: Icon(Icons.map), label: "Map"),
-      BottomNavigationBarItem(icon: Icon(Icons.people), label: "People"),
-    ];
+    void _onTapBottomNavBarItem(int index) {
+      setState(() {
+        _currentNavBarIndex = index;
+      });
+    }
 
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: bottomNavBarItems,
+        currentIndex: _currentNavBarIndex,
+        onTap: _onTapBottomNavBarItem,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
         ),
-        bottomNavigationBar: BottomNavigationBar(items: bottomNavBarItems),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ));
+      ),
+    );
   }
 }
