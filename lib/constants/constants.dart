4a36c372f105775081d79888bed6cee1a7948fab
James Jilhaney <notail2111@gmail.com>
2023-07-13 10:05:26 +0800

    Added constants file

---
+++ lib/constants/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Fonts
+const regularTextStyle = TextStyle();
+const titleTextStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+
+// Colors
+const bismarck = Color(0xff537188);
+const laser = Color(0xffCBB279);
+const bone = Color(0xffE1D4BB);
+const black = Color(0xffEEEEEE);
