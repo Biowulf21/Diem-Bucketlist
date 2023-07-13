99f5dc0bda09aef043ce99e957b26e3f1e5450dc
James Jilhaney <notail2111@gmail.com>
2023-07-13 10:06:05 +0800

    Created colorscheme from constants file

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: bismarck,
+            primary: bismarck,
+            secondary: laser,
+            tertiary: bone),
+        //color override
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
