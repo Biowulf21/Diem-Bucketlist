2503db5f8eb9b0b0a4207c96bc8e204af97c9621
James Jilhaney <notail2111@gmail.com>
2023-07-13 10:06:21 +0800

    Created bottom navigation bar for application

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:diem/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
@@ -51,11 +53,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    const bottomNavBarItems = <BottomNavigationBarItem>[
+      BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+      BottomNavigationBarItem(icon: Icon(Icons.map), label: "Map"),
+      BottomNavigationBarItem(icon: Icon(Icons.people), label: "People"),
+    ];
+
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
         ),
+        bottomNavigationBar: BottomNavigationBar(items: bottomNavBarItems),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
