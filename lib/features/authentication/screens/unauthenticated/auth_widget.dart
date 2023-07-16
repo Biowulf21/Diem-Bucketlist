adbb15a04f6b1667aca314a85f9c3ecd79ba6ed3
James Jilhaney <notail2111@gmail.com>
2023-07-16 22:20:56 +0800

    Changed futurebuilder to streambuilder for auth widget

--- lib/features/authentication/screens/unauthenticated/auth_widget.dart
+++ lib/features/authentication/screens/unauthenticated/auth_widget.dart
@@ -1,3 +1,4 @@
+import 'package:diem/features/authentication/repositories/auth.dart';
 import 'package:diem/features/authentication/screens/unauthenticated/login_page.dart';
 import 'package:diem/main.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -13,23 +14,42 @@ class AuthWidget extends StatefulWidget {
 class _AuthWidgetState extends State<AuthWidget> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<User?>(
-      builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
-        } else if (snapshot.hasData) {
-          final user = snapshot.data;
-          if (user == null) {
-            return LoginPage();
+    return StreamBuilder(
+        stream: Auth().authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasData) {
+            final user = snapshot.data;
+            if (user == null) {
+              return const LoginPage();
+            } else {
+              return const MyHomePage();
+            }
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
           } else {
-            return MyHomePage();
+            return const LoginPage();
           }
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          return LoginPage();
-        }
-      },
-    );
+        });
   }
 }
+// FutureBuilder<User?>(
+//       builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return CircularProgressIndicator();
+//         } else if (snapshot.hasData) {
+//           final user = snapshot.data;
+//           if (user == null) {
+//             return LoginPage();
+//           } else {
+//             return MyHomePage();
+//           }
+//         } else if (snapshot.hasError) {
+//           return Text('Error: ${snapshot.error}');
+//         } else {
+//           return LoginPage();
+//         }
+//       },
+//     );
+//
