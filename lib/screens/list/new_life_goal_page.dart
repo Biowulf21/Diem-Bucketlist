ea11ef3d855c12a489269f1d25e2e556d2183234
James Jilhaney <notail2111@gmail.com>
2023-07-16 23:03:32 +0800

    Changed to textformfields and changed into listview

--- lib/screens/list/new_life_goal_page.dart
+++ lib/screens/list/new_life_goal_page.dart
@@ -10,11 +10,25 @@ class NewLifeGoalPage extends StatefulWidget {
 class _NewLifeGoalPageState extends State<NewLifeGoalPage> {
   TextEditingController _goalTitleController = TextEditingController();
   TextEditingController _goalDescriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        children: [TextField(), TextField()],
+    return Center(
+      child: Form(
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: _goalTitleController,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(controller: _goalDescriptionController),
+            ),
+          ],
+        ),
       ),
     );
   }
