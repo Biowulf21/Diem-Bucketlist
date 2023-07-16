58cee82487b596a993c7c4d9fc7f56e52041b928
James Jilhaney <notail2111@gmail.com>
2023-07-16 22:50:35 +0800

    Renamed and moved life_item widget into bucketlist functionality

--- lib/features/bucket_list/repositories/life_goals/widgets/life_goal_widget.dart
+++ lib/features/bucket_list/repositories/life_goals/widgets/life_goal_widget.dart
@@ -1,8 +1,8 @@
 import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
 import 'package:flutter/material.dart';
 
-class LifeItemWidget extends StatelessWidget {
-  const LifeItemWidget({super.key, required this.item});
+class LifeGoalWidget extends StatelessWidget {
+  const LifeGoalWidget({super.key, required this.item});
   final LifeItem item;
 
   @override
