9566819ce41125318f88689603446e8b9148c482
James Jilhaney <notail2111@gmail.com>
2023-07-19 15:12:10 +0800

    Added function to create a chip for categories

--- lib/main.dart
+++ lib/main.dart
@@ -158,4 +158,17 @@ class _MyHomePageState extends State<MyHomePage> {
       },
     );
   }
+  Widget _buildChip(String label) {
+    return Chip(
+      labelPadding: EdgeInsets.all(2.0),
+      avatar: CircleAvatar(
+        backgroundColor: Colors.white70,
+        child: Text(label[0].toUpperCase()),
+      ),
+      label: Text(label),
+      elevation: 6.0,
+      shadowColor: Colors.grey[60],
+      padding: EdgeInsets.all(8.0),
+    );
+  }
 }
