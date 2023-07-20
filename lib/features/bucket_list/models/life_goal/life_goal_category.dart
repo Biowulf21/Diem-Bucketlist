4519fd24314f18dd90c84f30c74f5482b5ccf87c
James Jilhaney <notail2111@gmail.com>
2023-07-20 11:23:23 +0800

    Changed lifegoalcategory to be own class instead of enum

--- lib/features/bucket_list/models/life_goal/life_goal_category.dart
+++ lib/features/bucket_list/models/life_goal/life_goal_category.dart
@@ -1,1 +1,13 @@
-enum LifeGoalCategory { family, travel, career, health, growth, financial }
+import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
+
+class LifeGoalCategory {
+  final String label;
+  final String id;
+  final String? description;
+
+  LifeGoalCategory({required this.label, required this.id, this.description});
+
+  String get getLabel => label;
+  String get getId => id;
+  String? get getDescription => description;
+}
