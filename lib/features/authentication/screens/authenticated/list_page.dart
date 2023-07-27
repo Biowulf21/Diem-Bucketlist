24e4818685a61aef82a0a7bc6cebe816989573d2
James Jilhaney <notail2111@gmail.com>
2023-07-27 20:23:00 +0800

    Connected lifegoal provider with displaying lifegoal widgets

--- lib/features/authentication/screens/authenticated/list_page.dart
+++ lib/features/authentication/screens/authenticated/list_page.dart
@@ -1,20 +1,25 @@
 import 'package:diem/features/authentication/repositories/auth.dart';
+import 'package:diem/features/bucket_list/providers/bucketlist_item_providers.dart';
+import 'package:diem/features/bucket_list/repositories/life_goals/widgets/life_goal_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ListPage extends StatefulWidget {
-  const ListPage({super.key});
+class ListPage extends ConsumerStatefulWidget {
+  const ListPage({Key? key}) : super(key: key);
 
   @override
-  State<ListPage> createState() => _ListPageState();
+  ListPageState createState() => ListPageState();
 }
 
-class _ListPageState extends State<ListPage> {
+class ListPageState extends ConsumerState<ListPage> {
   @override
   Widget build(BuildContext context) {
-    List<Widget> listItems = <Widget>[Text('otin')];
+    final counter = ref.watch(lifeGoalListProvider);
+    List<LifeGoalWidget> lifeGoals =
+        counter.map((goal) => LifeGoalWidget(item: goal)).toList();
 
     return ListView(
-      children: [],
+      children: lifeGoals,
     );
   }
 }
--- lib/features/bucket_list/providers/bucketlist_item_providers.dart
+++ lib/features/bucket_list/providers/bucketlist_item_providers.dart
@@ -2,4 +2,10 @@ import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final StateProvider<List<LifeGoal>> lifeGoalListProvider =
-    StateProvider<List<LifeGoal>>((ref) => []);
+    StateProvider<List<LifeGoal>>((ref) => [
+          LifeGoal(
+              id: "Sample id",
+              title: "Sample title",
+              description: "sample desciption",
+              isCompleted: false)
+        ]);
