ed0a7ea2d506ac6f954f8b00d5f64d027d23b682
James Jilhaney <notail2111@gmail.com>
2023-07-27 23:34:07 +0800

    Created local/sqlite data source for life goals

---
+++ lib/features/bucket_list/repositories/life_goals/data_sources/local_file_source_life_goal.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+abstract class LocalDataSourceLifeGoalInterface {}
+
+class LocalDataSourceLifeGoalImpl implements LocalDataSourceLifeGoalInterface {
+  Database? _instance;
+  Future<Database> get database async {
+    if (_instance != null) return _instance!;
+
+    _instance = await _initDB('todo.db');
+    return _instance!;
+  }
+
+// LocalDataSourceLifeGoalInterface._privateConstructor();
+//   static final LocalDataSourceLifeGoalInterface instance = LocalDataSourceLifeGoalInterface._privateConstructor();
+
+  Future<Database> _initDB(String filePath) async {
+    Directory dbDirectory = await getApplicationSupportDirectory();
+    String path = join(dbDirectory.path, 'life_goal.db');
+    return await openDatabase(path, version: 1, onCreate: _onCreateDb);
+  }
+
+  FutureOr<void> _onCreateDb(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE life_goals()
+      id INTEGER PRIMARY KEY,
+      title TEXT,
+      description TEXT,
+      isCompleted INTEGER
+    )
+    ''');
+  }
+
+// CRUD OPERATIONS
+  Future<List<LifeGoal>> getLifeGoals() async {
+    Database db = await database;
+    var life_goals = await db.query('life_goals', orderBy: 'date_created');
+  }
+}
