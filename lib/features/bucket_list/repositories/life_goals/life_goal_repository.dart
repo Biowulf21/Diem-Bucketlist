279943c953cef0a82e695df0bd7433617a19de2f
James Jilhaney <notail2111@gmail.com>
2023-07-28 17:29:34 +0800

    Changed function to fetchLifeGoals

--- lib/features/bucket_list/repositories/life_goals/life_goal_repository.dart
+++ lib/features/bucket_list/repositories/life_goals/life_goal_repository.dart
@@ -1,8 +1,9 @@
 import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
+import 'package:flutter/material.dart';
 
 abstract class LifeGoalRepository {
   Future<void> addLifeItem(LifeGoal lifeItem);
-  Future<List<LifeGoal>> fetchLifeItems();
+  Future<List<LifeGoal>> fetchLifeGoals();
   Future<void> updateLifeItem(String id, bool isCompleted);
   Future<void> deleteLifeItem(String id);
 }
