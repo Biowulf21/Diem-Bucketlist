72b956faf623a04aa1113151cbeea75d35820b26
James Jilhaney <notail2111@gmail.com>
2023-07-28 23:36:42 +0800

    Created DTO for life_goal_category

--- lib/features/bucket_list/models/life_goal_category/life_goal_category.dart
+++ lib/features/bucket_list/models/life_goal_category/life_goal_category.dart
@@ -1,13 +1,23 @@
 import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
 
 class LifeGoalCategory {
-  final String label;
-  final String id;
-  final String? description;
+  String label;
+  String id;
+  String? description;
 
   LifeGoalCategory({required this.label, required this.id, this.description});
 
   String get getLabel => label;
   String get getId => id;
   String? get getDescription => description;
+
+  factory LifeGoalCategory.fromJson(Map<String, dynamic> json) =>
+      LifeGoalCategory(
+        label: json['label'],
+        id: json['id'],
+        description: json['description'],
+      );
+
+  Map<String, dynamic> toJson() =>
+      {'id': id, 'label': label, 'description': description};
 }
