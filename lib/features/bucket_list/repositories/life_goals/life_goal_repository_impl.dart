2a0e2bb9a5e493947503967f2a8bf4328c12c84c
James Jilhaney <notail2111@gmail.com>
2023-07-28 17:29:56 +0800

    fetchLifeGoals from local db before getting it from cloud db

--- lib/features/bucket_list/repositories/life_goals/life_goal_repository_impl.dart
+++ lib/features/bucket_list/repositories/life_goals/life_goal_repository_impl.dart
@@ -1,25 +1,34 @@
 import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
+import 'package:diem/features/bucket_list/repositories/life_goals/data_sources/firebase_datasource_life_item.dart';
+import 'package:diem/features/bucket_list/repositories/life_goals/data_sources/local_file_source_life_goal.dart';
 import 'package:diem/features/bucket_list/repositories/life_goals/life_goal_repository.dart';
 
 class LifeGoalImplRepository implements LifeGoalRepository {
+  @override
   Future<void> addLifeItem(LifeGoal lifeItem) async {
     return Future(() => null);
   }
 
-  Future<List<LifeGoal>> fetchLifeItems() {
-    return Future(() => [
-          LifeGoal(
-              id: "hehe",
-              title: "try lang",
-              description: "dse",
-              isCompleted: true)
-        ]);
+  @override
+  Future<List<LifeGoal>> fetchLifeGoals() async {
+    List<LifeGoal> lifeGoalsFromLocalDataSource =
+        await LocalDataSourceLifeGoalImpl().getLifeGoals();
+
+    if (lifeGoalsFromLocalDataSource.isEmpty) {
+      List<LifeGoal> lifeGoalsFromCloud =
+          await FirebaseDataSourceLifeGoal().getLifeGoals();
+
+      return lifeGoalsFromCloud;
+    }
+    return lifeGoalsFromLocalDataSource;
   }
 
+  @override
   Future<void> updateLifeItem(String id, bool isCompleted) {
     return Future(() => null);
   }
 
+  @override
   Future<void> deleteLifeItem(String id) {
     return Future(() => null);
   }
