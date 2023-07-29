0df304fa28c51fd9281cbaa650ebd689b22acaa6
James Jilhaney <notail2111@gmail.com>
2023-07-29 11:13:03 +0800

    Created singleton class for creating local database and its tables

---
+++ lib/utils/database/local_db_singleton.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
+import 'package:diem/utils/database/life_goals/abstract_data_source.dart';
+import 'package:diem/utils/firebase_doc_id_generator.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class LocalDBSingleton {
+  Database? _instance;
+
+  Future<Database> get database async {
+    if (_instance != null) return _instance!;
+
+    _instance = await _initDB('main.db');
+    return _instance!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    Directory dbDirectory = await getApplicationSupportDirectory();
+    String path = join(dbDirectory.path, filePath);
+    return await openDatabase(path, version: 1, onCreate: _onCreateDb);
+  }
+
+  FutureOr<void> _onCreateDb(Database db, int version) async {
+    await _createLifeGoalTable(db);
+    await _createLifeGoalCategoriesTable(db);
+    await _createForSynchTable(db);
+  }
+
+  _createLifeGoalTable(Database db) async {
+    await db.execute('''
+    CREATE TABLE life_goals(
+      id INTEGER PRIMARY KEY,
+      firebaseID VARCHAR(20) NOT NULL,
+      title TEXT NOT NULL,
+      description TEXT NOT NULL,
+      isCompleted BOOLEAN NOT NULL,
+      dateCreated TIMESTAMP NOT NULL,
+      isDeleted BOOLEAN NULL,
+      dateDeleted TIMESTAMP NULL,
+      location VARCHAR(255) NULL,
+      notes TEXT NULL,
+      image VARCHAR(255) NULL
+    )
+    ''');
+  }
+
+  _createLifeGoalCategoriesTable(Database db) {}
+
+  _createForSynchTable(Database db) async {
+    await db.execute('''
+    CREATE TABLE for_synch(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      life_goal_id INTEGER, -- Define the column first
+      FOREIGN KEY (life_goal_id) REFERENCES life_goals(id) 
+    )
+    ''');
+  }
+}
