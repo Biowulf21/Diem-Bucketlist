fc2aa07978641ea58702b58e0b79fce885a1ccf9
James Jilhaney <notail2111@gmail.com>
2023-07-29 18:56:34 +0800

    updated provider for local life goal categories

--- lib/features/bucket_list/providers/life_goal_category_provider.dart
+++ lib/features/bucket_list/providers/life_goal_category_provider.dart
@@ -1,52 +1,55 @@
 import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
 import 'package:diem/features/bucket_list/models/life_goal_category/life_goal_category.dart';
-import 'package:diem/utils/database/life_goal_categories/life_goal_category_local_db_helper.dart';
+import 'package:diem/features/database/local/life_goal_category/life_goal_category_local_db_helper.dart';
+import 'package:diem/features/database/local_db_singleton.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite/sqlite_api.dart';
 
-final defaultGoalCategoryProvider = StateProvider<List<String>>((ref) => [
-      'family',
-      'travel',
-      'health',
-      'finance',
-      'personal',
-      'career',
-      'education',
-      'creative',
-      'relationship',
-    ]);
+// final defaultGoalCategoryProvider = StateProvider<List<String>>((ref) => [
+//       'family',
+//       'travel',
+//       'health',
+//       'finance',
+//       'personal',
+//       'career',
+//       'education',
+//       'creative',
+//       'relationship',
+//     ]);
+//
+// final selectedCategoryNotifier = StateNotifierProvider.autoDispose<
+//     SelectedGoalCategoryNotifier, List<LifeGoalCategory>>(
+//   (ref) => SelectedGoalCategoryNotifier(),
+// );
+//
+// class SelectedGoalCategoryNotifier
+//     extends StateNotifier<List<LifeGoalCategory>> {
+//   SelectedGoalCategoryNotifier() : super([]);
+//
+//   void addSelectedCategory(LifeGoalCategory category) {
+//     state = [...state, category];
+//   }
+//
+//   List<LifeGoalCategory> removeFromSelectedCategory(
+//       LifeGoalCategory removeCategory) {
+//     state = [
+//       for (final category in state)
+//         if (category.label != removeCategory.label) category,
+//     ];
+//     return state;
+//   }
+//
+//   void clearSelectedCategories() {
+//     state = [];
+//   }
+//
+//   List<LifeGoalCategory> getSelectedCategories() {
+//     return state;
+//   }
+// }
 
-final selectedCategoryNotifier = StateNotifierProvider.autoDispose<
-    SelectedGoalCategoryNotifier, List<LifeGoalCategory>>(
-  (ref) => SelectedGoalCategoryNotifier(),
-);
-
-class SelectedGoalCategoryNotifier
-    extends StateNotifier<List<LifeGoalCategory>> {
-  SelectedGoalCategoryNotifier() : super([]);
-
-  void addSelectedCategory(LifeGoalCategory category) {
-    state = [...state, category];
-  }
-
-  List<LifeGoalCategory> removeFromSelectedCategory(
-      LifeGoalCategory removeCategory) {
-    state = [
-      for (final category in state)
-        if (category.label != removeCategory.label) category,
-    ];
-    return state;
-  }
-
-  void clearSelectedCategories() {
-    state = [];
-  }
-
-  List<LifeGoalCategory> getSelectedCategories() {
-    return state;
-  }
-}
-
-final FutureProvider<List<LifeGoalCategory>> localLifeGoalDB =
+final FutureProvider<List<LifeGoalCategory>> localLifeGoalCategoryProvider =
     FutureProvider<List<LifeGoalCategory>>((ref) async {
-  return await LifeGoalCategoryDBHelper().getLifeGoalCategories();
+  Database db = await LocalDBSingleton().database;
+  return await LifeGoalCategoryDBHelper(instance: db).getLifeGoalCategories();
 });
