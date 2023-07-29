a2b369a1251a38133108b5929aa8b6960388b53a
James Jilhaney <notail2111@gmail.com>
2023-07-29 17:26:39 +0800

    Created categories widget

---
+++ lib/features/bucket_list/widgets/life_goal_categories_widget.dart
@@ -0,0 +1,51 @@
+import 'package:diem/features/bucket_list/models/life_goal_category/life_goal_category.dart';
+import 'package:diem/features/database/local/life_goal_category/life_goal_category_local_db_helper.dart';
+import 'package:diem/features/database/local_db_singleton.dart';
+import 'package:diem/utils/widgets/custom_chip.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+class LifeGoalCategoriesWidget extends StatefulWidget {
+  LifeGoalCategoriesWidget({super.key});
+
+  @override
+  State<LifeGoalCategoriesWidget> createState() =>
+      _LifeGoalCategoriesWidgetState();
+}
+
+class _LifeGoalCategoriesWidgetState extends State<LifeGoalCategoriesWidget> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  Future<List<LifeGoalCategory>> getChipList() async {
+    Database db = await LocalDBSingleton().database;
+
+    List<LifeGoalCategory> categories =
+        await LifeGoalCategoryDBHelper(instance: db).getLifeGoalCategories();
+
+    return categories;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<LifeGoalCategory>>(
+        future: getChipList(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            List<CustomChip> customChipList =
+                snapshot.data!.map((e) => CustomChip(label: e.label)).toList();
+            return Wrap(
+              children: customChipList,
+            );
+          }
+        });
+  }
+}
