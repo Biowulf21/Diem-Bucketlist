2ede9c837d06ec799986058424c02b43d2567d35
James Jilhaney <notail2111@gmail.com>
2023-08-05 17:56:34 +0800

    removed id in life_goal_category

--- lib/features/bucket_list/models/life_goal_category/life_goal_category.dart
+++ lib/features/bucket_list/models/life_goal_category/life_goal_category.dart
@@ -1,31 +1,21 @@
-
 class LifeGoalCategory {
   String label;
   String firebaseID;
   String? description;
 
   LifeGoalCategory(
-      {required this.label,
-      required this.id,
-      required this.firebaseID,
-      this.description});
+      {required this.label, required this.firebaseID, this.description});
 
   String get getLabel => label;
-  int get getId => id;
   String? get getDescription => description;
 
   factory LifeGoalCategory.fromJson(Map<String, dynamic> json) =>
       LifeGoalCategory(
         firebaseID: json['firebaseID'],
         label: json['label'],
-        id: json['id'],
         description: json['description'],
       );
 
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'label': label,
-        'firebaseID': firebaseID,
-        'description': description
-      };
+  Map<String, dynamic> toJson() =>
+      {'label': label, 'firebaseID': firebaseID, 'description': description};
 }
