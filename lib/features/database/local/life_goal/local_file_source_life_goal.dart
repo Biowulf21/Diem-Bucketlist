eb734464d8f506d6bede2f376f94633b796120e5
James Jilhaney <notail2111@gmail.com>
2023-08-05 17:57:00 +0800

    removed id from local data source of lifegoals

--- lib/features/database/local/life_goal/local_file_source_life_goal.dart
+++ lib/features/database/local/life_goal/local_file_source_life_goal.dart
@@ -1,11 +1,7 @@
 import 'dart:async';
-import 'dart:js_interop';
 
 import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
-import 'package:diem/features/bucket_list/models/life_goal_category/life_goal_category.dart';
-import 'package:diem/features/bucket_list/models/life_goal_category_relationship/life_goal_relationship_category.dart';
 import 'package:diem/features/database/abstract/abstract_data_source.dart';
-import 'package:diem/features/database/local/life_goal_category_relationship/life_goal_category_relationship.dart';
 import 'package:sqflite/sqflite.dart';
 
 class LocalDataSourceLifeGoalImpl implements AbstractDataSource {
@@ -48,41 +44,28 @@ class LocalDataSourceLifeGoalImpl implements AbstractDataSource {
   @override
   Future<int> addLifeGoal(LifeGoal lifeGoal) async {
     Database db = instance;
-    List<LifeGoalCategory>? categories = lifeGoal.categories;
-    //TODO: change this signature to List<LifeGoalCategory?> instead. This cannot be null
-
-    if (categories.isNull) throw Exception('Categories cannot be null');
-
-    categories!.forEach((category) {
-      LifeGoalCategoryRelationship relationship = LifeGoalCategoryRelationship(
-          firebaseID: category.firebaseID,
-          goalID: lifeGoal.id,
-          categoryID: category.id);
-      LifeGoalCategoryRelationshipDBHelper(instance: db)
-          .createLifeGoalCategoryRelationship(category);
-    });
-
-    return await db.insert('life_goals', lifeGoal.toJson());
+    return await db.insert('life_goals', lifeGoal.toJsonNoCategories());
   }
 
   @override
   Future<int> removeLifeGoal(String id) async {
     Database db = instance;
-    return await db.delete('life_goals', where: 'id = ?', whereArgs: [id]);
+    return await db
+        .delete('life_goals', where: 'firebaseID = ?', whereArgs: [id]);
   }
 
   @override
   Future<int> updateLifeGoal(LifeGoal lifeGoal) async {
     Database db = instance;
     return await db.update('life_goals', lifeGoal.toJson(),
-        where: 'id = ?', whereArgs: [lifeGoal.id]);
+        where: 'firebaseID = ?', whereArgs: [lifeGoal.firebaseID]);
   }
 
   @override
   Future<LifeGoal> getLifeGoal(String id) async {
     Database db = instance;
     var lifeGoalFromDB =
-        await db.query('life_goal', where: 'id = ?', whereArgs: [id]);
+        await db.query('life_goals', where: 'firebaseID = ?', whereArgs: [id]);
     LifeGoal lifeGoal = LifeGoal.fromJson(lifeGoalFromDB.first);
     return lifeGoal;
   }
