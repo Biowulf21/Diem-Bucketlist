485b2d04aa9882e5069942c3828fb91f05adba62
James Jilhaney <notail2111@gmail.com>
2023-08-07 17:12:19 +0800

    Create category relationships when creating lifegoal in its repository

--- lib/features/bucket_list/repositories/life_goals/life_goal_repository.dart
+++ lib/features/bucket_list/repositories/life_goals/life_goal_repository.dart
@@ -1,9 +1,14 @@
 import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
-import 'package:flutter/material.dart';
+import 'package:diem/features/bucket_list/models/life_goal_category/life_goal_category.dart';
+import 'package:sqflite/sqlite_api.dart';
 
 abstract class LifeGoalRepository {
-  Future<void> addLifeItem(LifeGoal lifeItem);
+  Future<void> addLifeGoal(
+      {required String title,
+      required String description,
+      required List<LifeGoalCategory> selectedCategories,
+      required Database db});
   Future<List<LifeGoal>> fetchLifeGoals();
-  Future<void> updateLifeItem(String id, bool isCompleted);
-  Future<void> deleteLifeItem(String id);
+  Future<void> updateLifeGoal(String id, bool isCompleted);
+  Future<void> deleteLifeGoal(String id);
 }
--- lib/features/bucket_list/repositories/life_goals/life_goal_repository_impl.dart
+++ lib/features/bucket_list/repositories/life_goals/life_goal_repository_impl.dart
@@ -1,18 +1,63 @@
+import 'package:diem/features/bucket_list/builders/life_goal_builder.dart';
 import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
+import 'package:diem/features/bucket_list/models/life_goal_category/life_goal_category.dart';
+import 'package:diem/features/bucket_list/models/life_goal_category_relationship/life_goal_relationship_category.dart';
 import 'package:diem/features/bucket_list/repositories/life_goals/life_goal_repository.dart';
 import 'package:diem/features/database/local/life_goal/local_file_source_life_goal.dart';
+import 'package:diem/features/database/local/life_goal_category/life_goal_category_local_db_helper.dart';
+import 'package:diem/features/database/local/life_goal_category_relationship/life_goal_category_relationship.dart';
 import 'package:diem/features/database/local_db_singleton.dart';
 import 'package:diem/features/database/remote/firebase_datasource_life_item.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:diem/utils/firebase_doc_id_generator.dart';
 import 'package:sqflite/sqlite_api.dart';
-
-import '../../providers/life_goal_category_provider.dart';
 
 class LifeGoalImplRepository implements LifeGoalRepository {
   @override
-  Future<void> addLifeItem(LifeGoal lifeItem) async {
-    return Future(() => null);
+  Future<void> addLifeGoal(
+      {required String title,
+      required String description,
+      required List<LifeGoalCategory> selectedCategories,
+      required Database db}) async {
+    final String firebaseID = FirebaseDocIDGenerator.createRandomID();
+
+    LifeGoal goal = LifeGoalBuilder(
+            firebaseID: firebaseID,
+            title: title,
+            description: description,
+            isCompleted: false)
+        .addCategories(selectedCategories)
+        .build();
+
+    var res = await LocalDataSourceLifeGoalImpl(instance: db).addLifeGoal(goal);
+
+    if (goal.categories!.isNotEmpty) {
+      for (var category in goal.categories!) {
+        LifeGoalCategoryRelationship relationship =
+            LifeGoalCategoryRelationship(
+          goalID: goal.firebaseID,
+          categoryID: category.firebaseID,
+          firebaseID: FirebaseDocIDGenerator.createRandomID(),
+        );
+
+        var relationshipResult =
+            await LifeGoalCategoryRelationshipDBHelper(instance: db)
+                .createLifeGoalCategoryRelationship(relationship);
+      }
+    }
+
+    // if (goal.categories!.isNotEmpty) {
+    //   for (var category in goal.categories!) {
+    //
+    //     for (var relationship in relationships) {
+    //       // TODO: check if insertions of relationships fail
+    //       // and throw appropriate errors
+    //
+    //       var relationshipResult =
+    //           await LifeGoalCategoryRelationshipDBHelper(instance: db)
+    //               .createLifeGoalCategoryRelationship(relationship);
+    //     }
+    //   }
+    // }
   }
 
   @override
@@ -31,12 +76,12 @@ class LifeGoalImplRepository implements LifeGoalRepository {
   }
 
   @override
-  Future<void> updateLifeItem(String id, bool isCompleted) {
+  Future<void> updateLifeGoal(String id, bool isCompleted) {
     return Future(() => null);
   }
 
   @override
-  Future<void> deleteLifeItem(String id) {
+  Future<void> deleteLifeGoal(String id) {
     return Future(() => null);
   }
 }
