2b816a5118f512cd524a33c2d659eb94d9de15f6
James Jilhaney <notail2111@gmail.com>
2023-08-09 14:03:29 +0800

    commented out long press to delete life goal

--- lib/features/bucket_list/widgets/life_goal_widget.dart
+++ lib/features/bucket_list/widgets/life_goal_widget.dart
@@ -1,10 +1,7 @@
 import 'package:diem/features/bucket_list/models/life_goal/life_goal.dart';
-import 'package:diem/features/bucket_list/providers/bucketlist_item_providers.dart';
-import 'package:diem/features/database/local/life_goal/local_file_source_life_goal.dart';
-import 'package:diem/features/database/local_db_singleton.dart';
+import 'package:diem/features/bucket_list/widgets/life_goal_relationship_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sqflite/sqlite_api.dart';
 
 class LifeGoalWidget extends ConsumerWidget {
   const LifeGoalWidget({super.key, required this.item});
@@ -13,14 +10,14 @@ class LifeGoalWidget extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return GestureDetector(
-      onLongPress: () async {
-        Database db = await LocalDBSingleton().database;
-        LocalDataSourceLifeGoalImpl(instance: db)
-            .removeLifeGoal(item.firebaseID)
-            .then((value) {
-          ref.invalidate(lifeGoalListProvider);
-        });
-      },
+      // onLongPress: () async {
+      //   Database db = await LocalDBSingleton().database;
+      //   LocalDataSourceLifeGoalImpl(instance: db)
+      //       .removeLifeGoal(item.firebaseID)
+      //       .then((value) {
+      //     ref.invalidate(lifeGoalListProvider);
+      //   });
+      // },
       child: Card(
         child: ListTile(
             title: Text(item.title),
@@ -28,7 +25,7 @@ class LifeGoalWidget extends ConsumerWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(item.description),
-                Text(item.dateCreated.toString())
+                CategoriesWidget(categories: item.categories!)
               ],
             )),
       ),
