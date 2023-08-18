e304f857d8f5034ed4095360cf14ac3edb4140d8
James Jilhaney <notail2111@gmail.com>
2023-08-18 15:11:34 +0800

    Small changes

--- lib/features/synchronize/services/synchronize_service.dart
+++ lib/features/synchronize/services/synchronize_service.dart
@@ -1,11 +1,32 @@
-abstract class SynchronizeServiceAbstract{
+abstract class SynchronizeServiceAbstract {
   Future<void> _syncCategories();
   Future<void> _syncLifeGoals();
   Future<void> _syncLifeGoalCategoryRelationships();
   Future<void> synchronize();
+}
 
+class SynchronizeService implements SynchronizeServiceAbstract {
+  @override
+  Future<void> _syncCategories() {
+    // TODO: implement _syncCategories
+    throw UnimplementedError();
   }
 
-class SynchronizeService implements SynchronizeServiceAbstract{
-
+  @override
+  Future<void> _syncLifeGoalCategoryRelationships() {
+    // TODO: implement _syncLifeGoalCategoryRelationships
+    throw UnimplementedError();
   }
+
+  @override
+  Future<void> _syncLifeGoals() {
+    // TODO: implement _syncLifeGoals
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> synchronize() {
+    // TODO: implement synchronize
+    throw UnimplementedError();
+  }
+}
